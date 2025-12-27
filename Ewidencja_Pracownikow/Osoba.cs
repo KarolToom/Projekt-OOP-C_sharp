c26d81431ef69bcc09080b63e4fa5cef0bb207bb
KarolToom <Karol@DESKTOP>
2025-12-27 22:54:22 +0100

    Projekt - klasa Osoba (glowna)

---
+++ Ewidencja_Pracownikow/Osoba.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ewidencja_Pracownikow
+{
+    public abstract class Osoba
+    {
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Pesel { get; set; }
+
+        protected Osoba(string imie, string nazwisko, string pesel)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Pesel = pesel;
+        }
+
+        public virtual string WyswietlDane()
+        {
+            return $"Imię: {Imie}, Nazwisko: {Nazwisko}, PESEL: {Pesel}";
+        }
+    }
+}
