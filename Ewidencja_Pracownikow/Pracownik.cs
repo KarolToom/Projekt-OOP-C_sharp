0c1d0d637791defc0db085fa03f02b8808ed71d6
KarolToom <Karol@DESKTOP>
2025-12-27 23:56:28 +0100

    Klasa Osoba

---
+++ Ewidencja_Pracownikow/Pracownik.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ewidencja_Pracownikow
+{
+    public abstract class Pracownik:Osoba
+    {
+        public decimal PensjaZasadnicza { get; set; }
+
+        protected Pracownik(string imie, string nazwisko, string pesel, decimal pensja)
+            : base(imie, nazwisko, pesel)
+        {
+            PensjaZasadnicza = pensja;
+        }
+
+        public abstract decimal ObliczPremie();
+
+        public decimal ObliczWynagrodzenie()
+        {
+            return PensjaZasadnicza + ObliczPremie();
+        }
+    }
+}
