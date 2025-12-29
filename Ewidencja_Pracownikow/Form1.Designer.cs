0338290c4756a118a0c6b8fe4a37e99c1d2485af
KarolToom <Karol@DESKTOP>
2025-12-29 22:23:11 +0100

    Prototyp

--- Ewidencja_Pracownikow/Form1.Designer.cs
+++ Ewidencja_Pracownikow/Form1.Designer.cs
@@ -31,21 +31,25 @@
             dgvPracownicy = new DataGridView();
             btnOdswiez = new Button();
             btnDodaj = new Button();
+            btnUsun = new Button();
+            btnZapiszZmiany = new Button();
+            btnImport = new Button();
+            btnEksport = new Button();
             ((System.ComponentModel.ISupportInitialize)dgvPracownicy).BeginInit();
             SuspendLayout();
             // 
             // dgvPracownicy
             // 
             dgvPracownicy.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvPracownicy.Location = new Point(201, 98);
+            dgvPracownicy.Location = new Point(22, 41);
             dgvPracownicy.Name = "dgvPracownicy";
-            dgvPracownicy.Size = new Size(240, 150);
+            dgvPracownicy.Size = new Size(766, 235);
             dgvPracownicy.TabIndex = 0;
-            dgvPracownicy.CellContentClick += dataGridView1_CellContentClick;
+            dgvPracownicy.CellContentClick += dgvPracownicy_CellContentClick;
             // 
             // btnOdswiez
             // 
-            btnOdswiez.Location = new Point(123, 330);
+            btnOdswiez.Location = new Point(22, 330);
             btnOdswiez.Name = "btnOdswiez";
             btnOdswiez.Size = new Size(146, 23);
             btnOdswiez.TabIndex = 1;
@@ -55,7 +59,7 @@
             // 
             // btnDodaj
             // 
-            btnDodaj.Location = new Point(366, 330);
+            btnDodaj.Location = new Point(217, 330);
             btnDodaj.Name = "btnDodaj";
             btnDodaj.Size = new Size(165, 23);
             btnDodaj.TabIndex = 2;
@@ -63,14 +67,57 @@
             btnDodaj.UseVisualStyleBackColor = true;
             btnDodaj.Click += btnDodaj_Click;
             // 
+            // btnUsun
+            // 
+            btnUsun.Location = new Point(421, 330);
+            btnUsun.Name = "btnUsun";
+            btnUsun.Size = new Size(183, 23);
+            btnUsun.TabIndex = 3;
+            btnUsun.Text = "Usuń";
+            btnUsun.UseVisualStyleBackColor = true;
+            btnUsun.Click += btnUsun_Click;
+            // 
+            // btnZapiszZmiany
+            // 
+            btnZapiszZmiany.Location = new Point(619, 330);
+            btnZapiszZmiany.Name = "btnZapiszZmiany";
+            btnZapiszZmiany.Size = new Size(169, 23);
+            btnZapiszZmiany.TabIndex = 4;
+            btnZapiszZmiany.Text = "Zapisz zmiany";
+            btnZapiszZmiany.UseVisualStyleBackColor = true;
+            btnZapiszZmiany.Click += btnZapiszZmiany_Click;
+            // 
+            // btnImport
+            // 
+            btnImport.Location = new Point(116, 375);
+            btnImport.Name = "btnImport";
+            btnImport.Size = new Size(132, 23);
+            btnImport.TabIndex = 5;
+            btnImport.Text = "Import";
+            btnImport.UseVisualStyleBackColor = true;
+            // 
+            // btnEksport
+            // 
+            btnEksport.Location = new Point(333, 375);
+            btnEksport.Name = "btnEksport";
+            btnEksport.Size = new Size(126, 23);
+            btnEksport.TabIndex = 6;
+            btnEksport.Text = "Eksport";
+            btnEksport.UseVisualStyleBackColor = true;
+            btnEksport.Click += button1_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnEksport);
+            Controls.Add(btnImport);
             Controls.Add(btnDodaj);
             Controls.Add(btnOdswiez);
             Controls.Add(dgvPracownicy);
+            Controls.Add(btnUsun);
+            Controls.Add(btnZapiszZmiany);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)dgvPracownicy).EndInit();
@@ -82,5 +129,9 @@
         private DataGridView dgvPracownicy;
         private Button btnOdswiez;
         private Button btnDodaj;
+        private Button btnUsun;
+        private Button btnZapiszZmiany;
+        private Button btnImport;
+        private Button btnEksport;
     }
 }
--- Ewidencja_Pracownikow/Form1.cs
+++ Ewidencja_Pracownikow/Form1.cs
@@ -2,7 +2,6 @@ using Microsoft.Data.SqlClient;
 using System;
 using System.Data;
 using System.Windows.Forms;
-using static System.ComponentModel.Design.ObjectSelectorEditor;
 
 namespace Ewidencja_Pracownikow
 {
@@ -12,7 +11,9 @@ namespace Ewidencja_Pracownikow
         public Form1()
         {
             InitializeComponent();
+
         }
+
 
         private void ZaladujDane()
         {
@@ -39,7 +40,14 @@ namespace Ewidencja_Pracownikow
                     adapter.Fill(tabela);
 
                     dgvPracownicy.DataSource = tabela;
-                    dgvPracownicy.Columns["NazwaStanowiska"].HeaderText = "Stanowisko";
+                    dgvPracownicy.Columns["IdPracownika"].HeaderText = "ID";
+                    dgvPracownicy.Columns["IdPracownika"].Width = 40;
+                    dgvPracownicy.Columns["Imie"].HeaderText = "Imiê";
+                    dgvPracownicy.Columns["Nazwisko"].HeaderText = "Nazwisko";
+                    dgvPracownicy.Columns["PESEL"].HeaderText = "PESEL";
+                    dgvPracownicy.Columns["Stanowisko"].Width = 120;
+
+                    dgvPracownicy.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
                 }
                 catch (Exception ex)
                 {
@@ -48,26 +56,216 @@ namespace Ewidencja_Pracownikow
             }
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void dgvPracownicy_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
         }
 
         private void btnDodaj_Click(object sender, EventArgs e)
         {
             FormDodaj oknoDodawania = new FormDodaj();
 
-      
+
             oknoDodawania.ShowDialog();
 
-       
+
             ZaladujDane();
 
+        }
+
+        private void btnUsun_Click(object sender, EventArgs e)
+        {
+            if (dgvPracownicy.CurrentRow != null)
+            {
+
+                int id = (int)dgvPracownicy.CurrentRow.Cells["IdPracownika"].Value;
+                string nazwisko = dgvPracownicy.CurrentRow.Cells["Nazwisko"].Value.ToString();
+
+                var dialog = MessageBox.Show($"Czy na pewno usun¹æ pracownika {nazwisko}?", "PotwierdŸ", MessageBoxButtons.YesNo);
+                if (dialog == DialogResult.Yes)
+                {
+                    try
+                    {
+                        using (SqlConnection conn = new SqlConnection(connectionString))
+                        {
+                            conn.Open();
+                            string sql = "DELETE FROM Pracownicy WHERE IdPracownika = @id";
+                            SqlCommand cmd = new SqlCommand(sql, conn);
+                            cmd.Parameters.AddWithValue("@id", id);
+                            cmd.ExecuteNonQuery();
+                        }
+                        ZaladujDane();
+                    }
+                    catch (Exception ex) { MessageBox.Show(ex.Message); }
+                }
+            }
+        }
+
+        private void btnZapiszZmiany_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dgvPracownicy.CurrentRow != null)
+                {
+                    int id = (int)dgvPracownicy.CurrentRow.Cells["IdPracownika"].Value;
+                    string imie = dgvPracownicy.CurrentRow.Cells["Imie"].Value.ToString();
+                    string nazwisko = dgvPracownicy.CurrentRow.Cells["Nazwisko"].Value.ToString();
+
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        string sql = "UPDATE Pracownicy SET Imie=@imie, Nazwisko=@nazwisko WHERE IdPracownika=@id";
+                        SqlCommand cmd = new SqlCommand(sql, conn);
+                        cmd.Parameters.AddWithValue("@imie", imie);
+                        cmd.Parameters.AddWithValue("@nazwisko", nazwisko);
+                        cmd.Parameters.AddWithValue("@id", id);
+                        cmd.ExecuteNonQuery();
+                    }
+                    MessageBox.Show("Dane zaktualizowane!");
+                    ZaladujDane();
+                }
+            }
+            catch (Exception ex) { MessageBox.Show("B³¹d edycji: " + ex.Message); }
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Pliki CSV (*.csv)|*.csv";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                int dodano = 0;
+                int pominieto = 0;
+                int bledy = 0;
+
+                try
+                {
+                    string[] linie = System.IO.File.ReadAllLines(ofd.FileName);
+
+                    for (int i = 1; i < linie.Length; i++)
+                    {
+                        try
+                        {
+                            string[] dane = linie[i].Split(';'); // Rozdzielamy œrednikiem
+
+                            if (dane.Length < 5) { bledy++; continue; }
+
+                            string imie = dane[0];
+                            string nazwisko = dane[1];
+                            string pesel = dane[2];
+                            decimal pensja = decimal.Parse(dane[3]);
+                            string stanowisko = dane[4];
+
+                            if (CzyPeselIstnieje(pesel))
+                            {
+                                pominieto++;
+                                continue;
+                            }
+
+                            int idStanowiska = 1; // domyœlnie Kierowca
+                            if (stanowisko == "Handlowiec") idStanowiska = 2;
+                            else if (stanowisko == "Manager") idStanowiska = 3;
+
+                        
+                            ZapiszDoBazyZImportu(imie, nazwisko, pesel, pensja, idStanowiska);
+                            dodano++;
+                        }
+                        catch (Exception)
+                        {
+                            bledy++; // Np. b³¹d parsowania liczby lub walidacja klasy Osoba
+                        }
+                    }
+                    ZaladujDane();
+                    MessageBox.Show($"Import zakoñczony!\nDodano: {dodano}\nPominiêto (duplikaty): {pominieto}\nB³êdy danych: {bledy}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("B³¹d odczytu pliku: " + ex.Message);
+                }
+            }
+        }
+
+        private bool CzyPeselIstnieje(string pesel)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM Pracownicy WHERE PESEL = @p", conn);
+                cmd.Parameters.AddWithValue("@p", pesel);
+                int count = (int)cmd.ExecuteScalar();
+                return count > 0;
+            }
+        }
+
+        private void ZapiszDoBazyZImportu(string i, string n, string p, decimal pensja, int idS)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "INSERT INTO Pracownicy (Imie, Nazwisko, PESEL, PensjaPodstawowa, IdDzialu, IdStanowiska) " +
+                             "VALUES (@i, @n, @p, @pensja, 1, @idS)";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@i", i);
+                cmd.Parameters.AddWithValue("@n", n);
+                cmd.Parameters.AddWithValue("@p", p);
+                cmd.Parameters.AddWithValue("@pensja", pensja);
+                cmd.Parameters.AddWithValue("@idS", idS);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void btnEksport_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Pliki CSV (*.csv)|*.csv";
+            sfd.FileName = "Eksport_Pracownikow_" + DateTime.Now.ToString("yyyyMMdd") + ".csv";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (System.IO.StreamWriter sw = new System.IO.StreamWriter(sfd.FileName, false, System.Text.Encoding.UTF8))
+                    {
+                        string naglowki = "";
+                        for (int i = 0; i < dgvPracownicy.Columns.Count; i++)
+                        {
+                            naglowki += dgvPracownicy.Columns[i].HeaderText + (i < dgvPracownicy.Columns.Count - 1 ? ";" : "");
+                        }
+                        sw.WriteLine(naglowki);
+
+                        
+                        foreach (DataGridViewRow row in dgvPracownicy.Rows)
+                        {
+                            if (!row.IsNewRow)
+                            {
+                                string linia = "";
+                                for (int i = 0; i < dgvPracownicy.Columns.Count; i++)
+                                {
+                                    
+                                    string wartosc = row.Cells[i].Value?.ToString() ?? "";
+                                    linia += wartosc + (i < dgvPracownicy.Columns.Count - 1 ? ";" : "");
+                                }
+                                sw.WriteLine(linia);
+                            }
+                        }
+                    }
+                    MessageBox.Show("Dane zosta³y pomyœlnie wyeksportowane do pliku!", "Sukces");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("B³¹d podczas eksportu: " + ex.Message);
+                }
+            }
         }
 
         private void btnOdswiez_Click(object sender, EventArgs e)
         {
             ZaladujDane();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
